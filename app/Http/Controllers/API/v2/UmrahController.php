4af92893b524789b93b15ca0b803cfe0840498b6
Mahmoud Tantawy <mahmoud.tantawy@robustastudio.com>
2016-04-07 00:22:53 +0200

    new umrah controller, umrah repository, umrahtest

---
+++ app/Http/Controllers/API/v2/UmrahController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers\API\v2;
+
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use App\Repositories\UmrahRepository;
+
+class UmrahController extends Controller
+{
+    protected $umrah;
+
+    public function __construct(UmrahRepository $umrah)
+    {
+        $this->umrah = $umrah;
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validator = \Validator::make($request->all(), [
+                'name' => 'required|max:255',
+                'sex' => 'required|in:male,female',
+                'age' => 'required|integer',
+                'country' => 'required|max:255',
+                'city' => 'required|max:255',
+                'death_cause' => 'required|string',
+                'death_date' => 'required|date',
+           ]);
+
+        if ($validator->fails()) {
+            return response()->json('Bad Request: Validation failed.', '400');
+        } else {
+            return \App\Deceased::Create(
+                array_merge(
+                    $request->all(),
+                    ['user_id' => \Authorizer::getResourceOwnerId()]
+                )
+            );
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ app/Repositories/UmrahRepository.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Repositories;
+
+use App\User;
+use App\Umrah;
+use App\Deceased;
+
+class UmrahRepository
+{
+    //
+}
