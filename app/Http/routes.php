60b4cf92fda8c8a30aa73f54d8adcc3995a23794
Mahmoud Tantawy <mahmoud.tantawy@robustastudio.com>
2016-05-02 21:18:31 +0200

    adding route for performed by me API

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -48,6 +48,7 @@ Route::group(['prefix' => 'api'], function () {
             Route::patch('users/me', 'UserController@update');
             // these have to be above the "resource" controller thingy to match requests first.
             Route::get('umrah/myrequests', ['as' => 'user.umrah.myrequests', 'uses' => 'UmrahController@myRequests']);
+            Route::get('umrah/performedbyme', ['as' => 'user.umrah.performedbyme', 'uses' => 'UmrahController@performedByMe']);
             Route::patch('umrah/{deceased}/updatestatus/{status}', ['as' => 'deceased.umrah.update', 'uses' => 'UmrahController@updateStatus']);
             Route::resource('umrah', 'UmrahController', ['except'   =>  ['create', 'edit', 'destroy']]);
         });
