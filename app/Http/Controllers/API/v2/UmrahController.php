7954766c44c6c46dad724c7dfae0d97420a23b84
Mahmoud Tantawy <mahmoud.tantawy@robustastudio.com>
2016-05-02 22:06:25 +0200

    getting umrahs performed by the authenticated user

--- app/Http/Controllers/API/v2/UmrahController.php
+++ app/Http/Controllers/API/v2/UmrahController.php
@@ -198,4 +198,34 @@ class UmrahController extends Controller
             ]);
         }
     }
+
+    public function performedByMe(Request $request)
+    {
+        // sorting
+        $sort_by = $request->has('sort_by') ? $request->input('sort_by') : 'created_at';
+        $sort = $request->has('sort') ? $request->input('sort') : 'desc';
+
+        // pagination
+        $per_page = $request->has('per_page') ? $request->input('per_page') : 10;
+
+        $deceased_list  = $this->umrah
+                                ->getUmrahsPerformedByMe()
+                                ->orderBy($sort_by, $sort)
+                                ->paginate($per_page);
+        $deceased_list->transform(function ($item, $key) {
+            $item->creator = $item->user;
+            $item->creator->user_id = $item->creator->id;
+            return $item;
+        });
+
+        // moving unset to another transform round to avoid unsetting any required objects
+        $deceased_list->transform(function ($item, $key) {
+            unset($item->user_id);
+            unset($item->user);
+            unset($item->creator->id);
+            return $item;
+        });
+
+        return $deceased_list;
+    }
 }
