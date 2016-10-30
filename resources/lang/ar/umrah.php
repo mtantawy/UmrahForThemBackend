6bc486696da4f82c305cfe97968fdcc7a323b324
Mahmoud Tantawy <mahmoud.tantawy@robustastudio.com>
2016-10-30 23:41:15 +0100

    Fixing AR message

--- resources/lang/ar/umrah.php
+++ resources/lang/ar/umrah.php
@@ -2,6 +2,6 @@
 
 return [
 
-    'umrah_being_performed_already_warning'  =>  'لقد قام متطوع آخر بالبدء في مناسك العمرة لهذه الحاله',
+    'umrah_being_performed_already_warning'  =>  'لقد قام متطوع آخر بالبدء في مناسك العمرة لهذه الحالmة',
 
 ];
